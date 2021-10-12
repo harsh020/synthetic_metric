2cf8593001dc8cf4995163808f0096aa023f9f62
Harsh Soni <devilincarcerated020@yahoo.com>
2021-10-13 01:00:10 +0530

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 
 setuptools.setup(
     name="synmetric",
-    version="0.2.dev0",
+    version="0.2.dev1",
     license='MIT',
     author="Harsh Soni",
     author_email="author@example.com",
     description="Metric to evaluate data quality for synthetic data.",
     url="https://github.com/harsh020/synthetic_metric",
-    download_url = 'https://github.com/harsh020/synthetic_metric/archive/v_02dev0.tar.gz',
+    download_url = 'https://github.com/harsh020/synthetic_metric/archive/v_02dev1.tar.gz',
     project_urls={
         "Bug Tracker": "https://github.com/harsh020/synthetic_metric/issues",
     },
@@ -19,8 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "synmetric"},
-    packages=setuptools.find_packages(where="synmetric"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires = [
         'numpy',

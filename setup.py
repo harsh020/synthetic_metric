3535c67bd597746a9cc628ab19d84f8e6fddbfca
Harsh Soni <devilincarcerated020@yahoo.com>
2021-10-12 20:46:39 +0530

    pypi setup

--- setup.py
+++ setup.py
@@ -3,15 +3,18 @@ import setuptools
 
 setuptools.setup(
     name="synmetric",
-    version="0.0.1",
+    version="0.1",
+    license='MIT',
     author="Harsh Soni",
     author_email="author@example.com",
     description="Metric to evaluate data quality for synthetic data.",
     url="https://github.com/harsh020/synthetic_metric",
+    download_url = 'https://github.com/harsh020/synthetic_metric/archive/v_01.tar.gz'
     project_urls={
         "Bug Tracker": "https://github.com/harsh020/synthetic_metric/issues",
     },
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",

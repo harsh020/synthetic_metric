f7e75680274658044693a68c6a44bce56b421201
Harsh Soni <devilincarcerated020@yahoo.com[credential]>
2022-10-02 21:24:22 +0530

    Export SVCMetric

--- synmetric/metric/__init__.py
+++ synmetric/metric/__init__.py
@@ -1,11 +1,13 @@
 from .base import BaseMetric
 from .model import RFCMetric, RFPrivacyMetric
+from .model import SVCMetric
 from .model import DiscriminatorMetric, PrivacyMetric
 
 from .statistical import CSTestMetric, KSTestMetric, StatisticalMetric
 
 __all__ = [
     RFCMetric,
+    SVCMetric,
     KSTestMetric,
     CSTestMetric,
     PrivacyMetric,
